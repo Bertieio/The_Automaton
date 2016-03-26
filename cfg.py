791fa25824df6557c605fd9ccc556504832b76c6
Bertie Scott <bertie@bertie.io>
2016-03-26 15:54:22 +0000

    Create cfg.py

---
+++ cfg.py
@@ -0,0 +1,9 @@
+HOST = "irc.twitch.tv"              # the Twitch IRC server                                                                                          
+PORT = 6667                         # always use port 6667!                                                                                          
+NICK = "the_automaton"            # your Twitch username, lowercase                                                                                  
+PASS = "oauth:" # your Twitch OAuth token                                                                              
+CHAN = "#steampunk_devil"                                                                                                                            
+                                                                                                                                                     
+                                                                                                                                                     
+DELAY = 0                                                                                                                                            
+RATE = (100/30) # messages per second    
