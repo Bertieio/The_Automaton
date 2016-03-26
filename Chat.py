da01e3406922370ae718dcbc922b471460338cd8
bertieio <bertie@bertie.io>
2016-03-26 16:07:28 +0000

    I hope this works 3

--- Chat.py
+++ Chat.py
@@ -1,3 +1,3 @@
-  import re, cfg
+import re, cfg, socket
 
 CHAT_RE_MESSAGE = re.compile(r"^:\w+!\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
--- cfg.py
+++
@@ -1,9 +0,0 @@
-HOST = "irc.twitch.tv"              # the Twitch IRC server
-PORT = 6667                         # always use port 6667!
-NICK = "the_automaton"            # your Twitch username, lowercase
-PASS = "oauth:BlahBlah" # your Twitch OAuth token                                                                              
-CHAN = "#steampunk_devil"
-
-
-DELAY = 0
-RATE = (100/30) # messages per second
