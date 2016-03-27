74c1029f533a7771c90f4a75cc4dd47bf5c84c17
bertieio <bertie@bertie.io>
2016-03-27 18:23:59 +0100

    worked on twitch api - each function now takes a twitch username as a args

--- TwitchAPI.py
+++ TwitchAPI.py
@@ -1,42 +1,48 @@
-import cfg, requests, time, dateutil.parser
+import requests, time, dateutil.parser
 
-def getMods():
-    response = requests.get("https://tmi.twitch.tv/group/user/" + cfg.CHANNAME + "/chatters").json()
+def getMods(user):
+    response = requests.get("https://tmi.twitch.tv/group/user/" + user + "/chatters").json()
     mods = response["chatters"]["moderators"]
     return mods
 
 
-def online():
-    response = requests.get("https://api.Twitch.tv/kraken/streams/" + cfg.CHANNAME).json()
+def online(user):
+    response = requests.get("https://api.Twitch.tv/kraken/streams/" + user).json()
     if response["stream"] is not None:
         return True
     else:
         return False
 
 
-def getGame():
-    online = online()
+def getGame(user):
+    online = online(user)
     if online == True:
-        game = getGameOnline()
+        game = getGameOnline(user)
     else:
-        game = getGameOffline()
+        game = getGameOffline(user)
     return game
 
 
-def getGameOnline():
-    response = requests.get("https://api.twitch.tv/kraken/streams/" + cfg.CHANNAME).json()
+def getGameOnline(user):
+    response = requests.get("https://api.twitch.tv/kraken/streams/" + user).json()
     if response["stream"] is not None:
         return response["stream"]["game"]
 
 
-def getGameOffline():
-        response = requests.get("https://api.twitch.tv/kraken/channels/" + cfg.CHANNAME).json()
-        if response["game"] is not None:
-            return ["game"]
+def getGameOffline(user):
+    response = requests.get("https://api.twitch.tv/kraken/channels/" + user).json()
+    if response["game"] is not None:
+        return ["game"]
 
 
-def Uptime():
-    response = requests.get("https://api.twitch.tv/kraken/streams/" + cfg.CHANNAME).json()
+def getChatters(user):
+    response = requests.get("https://tmi.twitch.tv/group/user/" + user + "/chatters").json()
+    chatters = response["chatters"]["moderators"] + response["chatters"]["staff"] + response["chatters"]["admins"] + response["chatters"]["global_mods"] + response["chatters"]["viewers"]
+    return chatters
+
+
+def Uptime(user):
+    response = requests.get("https://api.twitch.tv/kraken/streams/" + user).json()
     startTime = response["stream"]["created_at"]
     uptime = str(dateutil.parser.parse(startTime))
     compTime = int(time.time()) - (int(time.mktime(datetime.datetime.strptime(Uptime, '%Y-%m-%d %H:%M:%S+00:00').timetuple())))
